e2186e8d4f8d451a4b4f15fca7e0aec88b7dc711
KIrina06 <kirisha06@yandex.ru>
2023-09-28 11:52:54 +0300

    quest2

--- expertise/admin.py
+++ expertise/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from expertise.models import User, Status, Expertise, Application, ExpApp
+
+admin.site.register(User)
+admin.site.register(Status)
+admin.site.register(Expertise)
+admin.site.register(Application)
+admin.site.register(ExpApp)
--- expertise/models.py
+++ expertise/models.py
@@ -1,3 +1,31 @@
+from django import utils
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Status(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+
+class User(models.Model):
+    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100, null=True, blank=True)
+    status = models.CharField(max_length=20)
+
+class Expertise(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='static')
+    status = models.ForeignKey(to=Status, on_delete=models.PROTECT)
+
+class Application(models.Model):
+    status = models.ForeignKey(to=Status, on_delete=models.PROTECT)
+    date_of_create = models.DateField(default=utils.timezone.now)
+    date_of_form = models.DateField(default=utils.timezone.now)
+    date_of_finish = models.DateField(default=utils.timezone.now)
+    moderator = models.ForeignKey(to=User, on_delete=models.PROTECT)
+
+class ExpApp(models.Model):
+    expertise = models.ForeignKey(to=Expertise, on_delete=models.PROTECT)
+    application = models.ForeignKey(to=Application, on_delete=models.PROTECT)
+
