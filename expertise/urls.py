6c9dc7a6d2a405f5ed584e7c9062bc02a1f46632
KIrina06 <kirisha06@yandex.ru>
2023-09-28 08:22:20 +0300

    lab1

--- expertise/urls.py
+++ expertise/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GetOrders, GetOrder
+from .views import GetOrders, GetOrder, sendText
 
 urlpatterns = [
     path('', GetOrders),
-    path('order/<int:id>', GetOrder, name='order_url')
+    path('order/<int:id>', GetOrder, name='order_url'),
+    path('sendText', sendText, name='sendText'),
 ]
--- expertise/views.py
+++ expertise/views.py
@@ -3,20 +3,143 @@ from django.http import HttpResponse
 from datetime import date
 
 def GetOrders(request):
-    return render(request, 'orders.html', {'data' : {
-        'current_date': date.today(),
+    data = {
+        'title': 'Услуги',
         'orders': [
-            {'title': 'Проверка подлинности', 'id': 1},
-            {'title': 'Заказ копии', 'id': 2},
-            {'title': 'Определение художника', 'id': 3},
-            {'title': 'Определение эпохи', 'id': 4},
-            {'title': 'Реставрация', 'id': 5},
-            {'title': 'Заказ на разбор картины', 'id': 6},
-        ]
-    }})
+            {'id': 1, 
+             'name': 'Проверка подлинности',
+             'price': 10000,
+             'image': "https://mostmag.ru/wp-content/uploads/2020/11/a3028ac6-a555-4a08-9eda-2052f18af329.jpg",
+             'description': 'Проверка картины на подлинность. Срок проверки - 3 дня. В срочном порядке - 1 день.'},
+            {'id': 2, 
+             'name': 'Заказ копии',
+             'price': 21000,
+             'image': "https://mportret.ru/upload/medialibrary/33a/33af12aefdaa37207355dab3fc2c8afe.png",
+             'description': 'Срок создания копии от 7 дней до 3 месяцев в зависимости от размера и техники, в которой выполнена картина.'},
+            {'id': 3, 
+             'name': 'Определение художника',
+             'price': 4200,
+             'image': "https://dic.academic.ru/pictures/wiki/files/65/Adriaen_van_Ostade_006.jpg",
+             'description': 'Не можете определить чья работа попала к Вам в руки? Мы поможем!'},
+            {'id': 4, 
+             'name': 'Определение эпохи',
+             'price': 3700,
+             'image': 'https://yantar.ua/upload/custom/images/0_ac631_dc44a423_orig.jpg',
+             'description': 'Определение направления и жанра в котором писал художник. Сроки выполнения 3 - 5 дней.'},
+            {'id': 5, 
+             'name': 'Реставрация',
+             'price': 25000,
+             'image': 'https://antiquarika.ru/wp-content/uploads/2021/10/Restavratsiya-kartin.jpg',
+             'description': 'Реставрация картин, выполненных маслом.'},
+            {'id': 6, 
+             'name': 'Заказ на разбор картины',
+             'price': 5000,
+             'image': "https://s5.afisha.ru/mediastorage/e4/e1/8b1fb0681b304743b342ae72e1e4.jpg",
+             'description': 'Не знаете что за картина? Не проблема! Мы поможем узнать все о картине и ее создателе.'},
+        ]}
+    
+    return render(request, 'orders.html', data)
 
 def GetOrder(request, id):
-    return render(request, 'order.html', {'data' : {
-        'current_date': date.today(),
-        'id': id
-    }})
+    data = {
+        'title': 'Услуги',
+        'orders': [
+            {'id': 1, 
+             'name': 'Проверка подлинности',
+             'price': 10000,
+             'image': "https://mostmag.ru/wp-content/uploads/2020/11/a3028ac6-a555-4a08-9eda-2052f18af329.jpg",
+             'description': 'Проверка картины на подлинность. Срок проверки - 3 дня. В срочном порядке - 1 день.'},
+            {'id': 2, 
+             'name': 'Заказ копии',
+             'price': 21000,
+             'image': "https://mportret.ru/upload/medialibrary/33a/33af12aefdaa37207355dab3fc2c8afe.png",
+             'description': 'Срок создания копии от 7 дней до 3 месяцев в зависимости от размера и техники, в которой выполнена картина.'},
+            {'id': 3, 
+             'name': 'Определение художника',
+             'price': 4200,
+             'image': "https://dic.academic.ru/pictures/wiki/files/65/Adriaen_van_Ostade_006.jpg",
+             'description': 'Не можете определить чья работа попала к Вам в руки? Мы поможем!'},
+            {'id': 4, 
+             'name': 'Определение эпохи',
+             'price': 3700,
+             'image': 'https://yantar.ua/upload/custom/images/0_ac631_dc44a423_orig.jpg',
+             'description': 'Определение направления и жанра в котором писал художник. Сроки выполнения 3 - 5 дней.'},
+            {'id': 5, 
+             'name': 'Реставрация',
+             'price': 25000,
+             'image': 'https://antiquarika.ru/wp-content/uploads/2021/10/Restavratsiya-kartin.jpg',
+             'description': 'Реставрация картин, выполненных маслом.'},
+            {'id': 6, 
+             'name': 'Заказ на разбор картины',
+             'price': 5000,
+             'image': "https://s5.afisha.ru/mediastorage/e4/e1/8b1fb0681b304743b342ae72e1e4.jpg",
+             'description': 'Не знаете что за картина? Не проблема! Мы поможем узнать все о картине и ее создателе.'},
+        ]}
+    order = {
+        'id': id,
+        'name': 'name',
+        'image': 'image',
+        'price': 0,
+        'description': 'description',
+    }
+    for i in data['orders']:
+        if id == i['id']:
+            order['id'] = i['id']
+            order['name'] = i['name']
+            order['price'] = i['price']
+            order['image'] = i['image']
+            order['description'] = i['description']
+    
+    return render(request, 'order.html', order)
+
+def sendText(request):
+    input_text = request.POST.get('name', 'Проверка подлинности')
+    data = {
+        'title': 'Услуги',
+        'orders': [
+            {'id': 1, 
+             'name': 'Проверка подлинности',
+             'price': 10000,
+             'image': "https://mostmag.ru/wp-content/uploads/2020/11/a3028ac6-a555-4a08-9eda-2052f18af329.jpg",
+             'description': 'Проверка картины на подлинность. Срок проверки - 3 дня. В срочном порядке - 1 день.'},
+            {'id': 2, 
+             'name': 'Заказ копии',
+             'price': 21000,
+             'image': "https://mportret.ru/upload/medialibrary/33a/33af12aefdaa37207355dab3fc2c8afe.png",
+             'description': 'Срок создания копии от 7 дней до 3 месяцев в зависимости от размера и техники, в которой выполнена картина.'},
+            {'id': 3, 
+             'name': 'Определение художника',
+             'price': 4200,
+             'image': "https://dic.academic.ru/pictures/wiki/files/65/Adriaen_van_Ostade_006.jpg",
+             'description': 'Не можете определить чья работа попала к Вам в руки? Мы поможем!'},
+            {'id': 4, 
+             'name': 'Определение эпохи',
+             'price': 3700,
+             'image': 'https://yantar.ua/upload/custom/images/0_ac631_dc44a423_orig.jpg',
+             'description': 'Определение направления и жанра в котором писал художник. Сроки выполнения 3 - 5 дней.'},
+            {'id': 5, 
+             'name': 'Реставрация',
+             'price': 25000,
+             'image': 'https://antiquarika.ru/wp-content/uploads/2021/10/Restavratsiya-kartin.jpg',
+             'description': 'Реставрация картин, выполненных маслом.'},
+            {'id': 6, 
+             'name': 'Заказ на разбор картины',
+             'price': 5000,
+             'image': "https://s5.afisha.ru/mediastorage/e4/e1/8b1fb0681b304743b342ae72e1e4.jpg",
+             'description': 'Не знаете что за картина? Не проблема! Мы поможем узнать все о картине и ее создателе.'},
+        ]}
+    order = {
+        'id': 0,
+        'name': 'name',
+        'image': 'image',
+        'price': 0,
+        'description': 'description',
+    }
+    for i in data['orders']:
+        if input_text == i['name']:
+            order['id'] = i['id']
+            order['name'] = i['name']
+            order['price'] = i['price']
+            order['image'] = i['image']
+            order['description'] = i['description']
+    return render(request, 'order.html', order)
