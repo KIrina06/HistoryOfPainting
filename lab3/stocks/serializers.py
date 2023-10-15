9cfa9d616e82842f8ff42107edb15162c73ba0a7
KIrina06 <kirisha06@yandex.ru>
2023-10-16 02:26:01 +0300

    lab3

---
+++ lab3/stocks/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Expertise
+
+class ExpertiseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expertise
+        fields = ('name', 'description', 'price', 'status')
